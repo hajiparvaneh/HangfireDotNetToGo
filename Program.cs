3aa51cb974f4cbd982be48447c7ac08c968cc2bd
hajiparvaneh <55913813+hajiparvaneh@users.noreply.github.com>
2023-10-19 17:32:10 +0200

    docker compose + hangfire

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
+using Hangfire;
+using Hangfire.PostgreSql;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Adding Hangfire to services
+builder.Services.AddHangfire(config =>
+    config.UsePostgreSqlStorage(c =>
+        c.UseNpgsqlConnection(builder.Configuration.GetConnectionString("HangfireConnection"))));
+builder.Services.AddMvc(MvcOptions => MvcOptions.EnableEndpointRouting = false);
 
 var app = builder.Build();
 
@@ -14,12 +22,39 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHangfireDashboard();
+app.UseHangfireServer();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseStaticFiles();
 app.MapControllers();
 
+//Fire-and-Forget
+BackgroundJob.Enqueue(() => Console.WriteLine("Fire-and-forget"));
+
+//Delayed
+BackgroundJob.Schedule(() => Console.WriteLine("Delayed"), TimeSpan.FromDays(1));
+
+//Recurring
+RecurringJob.AddOrUpdate(() => Console.WriteLine("Minutely Job"), Cron.Minutely);
+
+//Continuation
+var id = BackgroundJob.Enqueue(() => Console.WriteLine("Hello, "));
+BackgroundJob.ContinueWith(id, () => Console.WriteLine("world!"));
+app.UseMvc(routes =>
+{
+    routes.MapRoute(
+        name: "default",
+        template: "{controller=Home}/{action=Index}/{id?}");
+});
 app.Run();
